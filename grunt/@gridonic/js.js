898d0e0ac75a78da2608ade6a0fa5ebafc3245ea
Sebastian Prein <basti@gridonic.ch>
2017-04-08 23:20:07 +0200

    Add foundation tasks

---
+++ grunt/@gridonic/js.js
@@ -0,0 +1,44 @@
+const cmds = {
+
+    // js:dev -> npm run build
+    get dev() { return this._build(); },
+
+    // js:prod -> npm run build:prod
+    get prod() { return this._build('prod'); },
+
+    // js:watch -> npm run watch
+    get watch() { return this._run('watch'); },
+
+    // js:test -> npm run test
+    get test() { return this._run('test'); },
+
+    // change into js directory, run yarn, run npm command…
+    _run(cmd) { return [ '(cd <%= src %>/js', 'yarn', `npm run ${cmd})`].join('&&'); },
+
+    // build task
+    _build(env) {
+        return [
+            this._run('build' + (env === 'prod' ? ':prod' : '')),
+
+            // make sure destination exists
+            'mkdir -pv <%= assets.js %>',
+
+            // copy build to destination
+            'cp -vr <%= src %>/js/build/* <%= assets.js %>'
+        ].join('&&');
+    }
+};
+
+module.exports = (grunt) => {
+    const task = {
+        name: 'js',
+        description: 'Bridge task to the build tools of our JavaScript foundation.',
+        fn(cmd = 'dev') {
+            grunt.log.writeln('👍  Using scripts from JavaScript foundation…')
+            grunt.config.merge({ shell: { js: { command: cmds[cmd] } } });
+            grunt.task.run('shell:js');
+        }
+    };
+
+    grunt.registerTask(task.name, task.description, task.fn);
+};
---
+++ grunt/@gridonic/sass.js
@@ -0,0 +1,50 @@
+const cmds = {
+
+    // sass:dev -> npm run build
+    get dev() { return this._build(); },
+
+    // sass:prod -> npm run build:prod
+    get prod() { return this._build('prod'); },
+
+    // sass:watch -> npm run watch
+    get watch() { return this._run('watch'); },
+
+    // sass:test -> npm run test
+    get test() { return this._run('test'); },
+
+    // sass:docs -> npm run docs
+    get docs() { return this._run('docs'); },
+
+    // sass:styleguide -> npm run styleguide
+    get styleguide() { return this._run('styleguide'); },
+
+    // change into sass directory, run yarn, run npm command…
+    _run(cmd) { return [ '(cd <%= src %>/sass', 'yarn', `npm run ${cmd})`].join('&&'); },
+
+    // build task
+    _build(env) {
+        return [
+            this._run('build' + (env === 'prod' ? ':prod' : '')),
+
+            // make sure destination exists
+            'mkdir -pv <%= assets.css %>',
+
+            // copy build to destination
+            'cp -vr <%= src %>/sass/build/* <%= assets.css %>'
+        ].join('&&');
+    }
+};
+
+module.exports = (grunt) => {
+    const task = {
+        name: 'sass',
+        description: 'Bridge task to the build tools of our Sass foundation.',
+        fn(cmd = 'dev') {
+            grunt.log.writeln('👍  Using scripts from Sass foundation…')
+            grunt.config.merge({ shell: { sass: { command: cmds[cmd] } } });
+            grunt.task.run('shell:sass');
+        }
+    };
+
+    grunt.registerTask(task.name, task.description, task.fn);
+};
