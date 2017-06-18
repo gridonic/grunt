eae736ecdf8d9527bbfe2f72f0553bb4b76b91b2
Sebastian Prein <basti@gridonic.ch>
2017-06-18 19:03:22 +0200

    Add watch task

---
+++ grunt/watch.js
@@ -0,0 +1,10 @@
+module.exports = {
+    js: {
+        files: ['<%= src %>/js/src/**/*.js'],
+        tasks: ['js']
+    },
+    sass: {
+        files: ['<%= src %>/sass/src/**/*.scss'],
+        tasks: ['sass']
+    }
+};
